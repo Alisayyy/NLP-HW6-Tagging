8825a1e3929786a5c38cd3ae740a71df5a6d2a00
Jingman Wang <jwang408@jhu.edu>
2023-11-21 13:22:09 -0500

    crf new files

---
+++ code/testcrf_en.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# This file illustrates how you might experiment with the HMM interface at the prompt.
+# You can also run it directly.
+
+import logging
+import math
+import os
+from pathlib import Path
+from typing import Callable
+
+from corpus import TaggedCorpus
+from eval import eval_tagging, model_cross_entropy, viterbi_error_rate
+from crf import CRF
+from lexicon import build_lexicon
+import torch
+
+# Set up logging
+log = logging.getLogger("test_en")       # For usage, see findsim.py in earlier assignment.
+logging.basicConfig(format="%(levelname)s : %(message)s", level=logging.INFO)  # could change INFO to DEBUG
+# torch.autograd.set_detect_anomaly(True)    # uncomment to improve error messages from .backward(), but slows down
+
+# Switch working directory to the directory where the data live.  You may want to edit this line.
+os.chdir("../data")
+entrain = TaggedCorpus(Path("ensup"), Path("enraw"))                               # all training
+ensup =   TaggedCorpus(Path("ensup"), tagset=entrain.tagset, vocab=entrain.vocab)  # supervised training
+endev =   TaggedCorpus(Path("endev"), tagset=entrain.tagset, vocab=entrain.vocab)  # evaluation
+log.info(f"Tagset: f{list(entrain.tagset)}")
+known_vocab = TaggedCorpus(Path("ensup")).vocab    # words seen with supervised tags; used in evaluation
+
+# Initialize an HMM
+lexicon = build_lexicon(entrain, embeddings_file=Path('words-50.txt'))  # works better with more attributes!
+hmm = CRF(entrain.tagset, entrain.vocab, lexicon)
+
+# Let's initialize with supervised training to approximately maximize the
+# regularized log-likelihood.  If you want to speed this up, you can increase
+# the tolerance of training (using the `tolerance` argument), since we don't 
+# really have to train to convergence.
+loss_sup = lambda model: model_cross_entropy(model, eval_corpus=ensup)
+hmm.train(corpus=ensup, loss=loss_sup, minibatch_size=30, evalbatch_size=10000, lr=0.0001, reg=1) 
+
+# Now let's throw in the unsupervised training data as well, and continue
+# training to try to improve accuracy on held-out development data.
+# We'll stop when accuracy is getting worse, so we can get away without regularization,
+# but it would be better to search for the best `reg` and other hyperparameters in this call.
+loss_dev = lambda model: viterbi_error_rate(model, eval_corpus=endev, known_vocab=known_vocab)
+hmm.train(corpus=entrain, loss=loss_dev, minibatch_size=30, evalbatch_size=10000, lr=0.0001, reg=0)
+
+# hmm = CRF.load(Path("en_hmm_awesome_v.pkl"), gpu=False)
+# More detailed look at the first 10 sentences in the held-out corpus,
+# including Viterbi tagging.
+for m, sentence in enumerate(endev):
+    if m >= 10: break
+    viterbi = hmm.viterbi_tagging(sentence.desupervise(), endev)
+    counts = eval_tagging(predicted=viterbi, gold=sentence, 
+                          known_vocab=known_vocab)
+    num = counts['NUM', 'ALL']
+    denom = counts['DENOM', 'ALL']
+    
+    log.info(f"Gold:    {sentence}")
+    log.info(f"Viterbi: {viterbi}")
+    log.info(f"Loss:    {denom - num}/{denom}")
+    log.info(f"Prob:    {math.exp(hmm.log_prob(sentence, endev))}")
---
+++ code/testcrf_ic.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+# This file illustrates how you might experiment with the HMM interface at the prompt.
+# You can also run it directly.
+
+import logging, math, os
+from pathlib import Path
+from typing import Callable
+
+from corpus import TaggedCorpus
+from eval import model_cross_entropy, write_tagging
+from crf import CRF
+from lexicon import build_lexicon
+import torch
+
+# Set up logging
+log = logging.getLogger("test_ic")       # For usage, see findsim.py in earlier assignment.
+logging.basicConfig(level=logging.INFO)  # could change INFO to DEBUG
+# torch.autograd.set_detect_anomaly(True)    # uncomment to improve error messages from .backward(), but slows down
+
+# Switch working directory to the directory where the data live.  You may want to edit this line.
+os.chdir("../data")
+
+# Make an HMM with randomly initialized parameters.
+icsup = TaggedCorpus(Path("icsup"), add_oov=False)
+log.info(f"Ice cream vocabulary: {list(icsup.vocab)}")
+log.info(f"Ice cream tagset: {list(icsup.tagset)}")
+lexicon = build_lexicon(icsup, one_hot=True)   # one-hot lexicon: separate parameters for each word
+hmm = CRF(icsup.tagset, icsup.vocab, lexicon)
+# hmm.A = torch.tensor([[0.8, 0.1, 0.1, 0],[0.1, 0.8, 0.1, 0.0],[0.0, 0.0, 0.0, 0.0], [0.5, 0.5, 0.0, 0.0]])   
+# hmm.B = torch.tensor([[0.7, 0.2, 0.1],[0.1, 0.2, 0.7],[0.0, 0.0, 0.0],[0.0, 0.0, 0.0]])   
+
+log.info("*** Current A, B matrices (computed by softmax from small random parameters)")
+hmm.updateAB()   # compute the matrices from the initial parameters (this would normally happen during training).
+                 # An alternative is to set them directly to some spreadsheet values you'd like to try.
+hmm.printAB()
+
+# While training on ice cream, we will just evaluate the cross-entropy
+# on the training data itself (icsup), since we are interested in watching it improve.
+log.info("*** Supervised training on icsup")
+cross_entropy_loss = lambda model: model_cross_entropy(model, icsup)
+hmm.train(corpus=icsup, loss=cross_entropy_loss, 
+          minibatch_size=10, evalbatch_size=500, lr=0.01, tolerance=0.0001)
+
+log.info("*** A, B matrices after training on icsup (should approximately "
+         "match initial params on spreadsheet [transposed])")
+# hmm.A = torch.tensor([[0.8, 0.1, 0.1, 0],[0.1, 0.8, 0.1, 0.0],[0.0, 0.0, 0.0, 0.0], [0.5, 0.5, 0.0, 0.0]])   
+# hmm.B = torch.tensor([[0.7, 0.2, 0.1],[0.1, 0.2, 0.7],[0.0, 0.0, 0.0],[0.0, 0.0, 0.0]])   
+hmm.printAB()
+
+# Since we used a low tolerance, that should have gotten us about up to the
+# initial parameters on the spreadsheet.  Let's tag the spreadsheet "sentence"
+# (that is, the sequence of ice creams) using the Viterbi algorithm.
+log.info("*** Viterbi results on icraw")
+icraw = TaggedCorpus(Path("icraw"), tagset=icsup.tagset, vocab=icsup.vocab)
+write_tagging(hmm, icraw, Path("icraw.output"))  # calls hmm.viterbi_tagging on each sentence
+os.system("cat icraw.output")   # print the file we just created, and remove it
+
+# Now let's use the forward algorithm to see what the model thinks about 
+# the probability of the spreadsheet "sentence."
+log.info("*** Forward algorithm on icraw (should approximately match iteration 0 "
+             "on spreadsheet)")
+for sentence in icraw:
+    prob = math.exp(hmm.log_prob(sentence, icraw))
+    log.info(f"{prob} = p({sentence})")
+
+# Finally, let's reestimate on the icraw data, as the spreadsheet does.
+log.info("*** Reestimating on icraw (perplexity should improve on every iteration)")
+negative_log_likelihood = lambda model: model_cross_entropy(model, icraw)  # evaluate on icraw itself
+hmm.train(corpus=icraw, loss=negative_log_likelihood,
+          minibatch_size=10, evalbatch_size=500, lr=0.001, tolerance=0.0001)
+
+log.info("*** A, B matrices after reestimation on icraw (SGD, not EM, but still "
+         "should approximately match final params on spreadsheet [transposed])")
+hmm.printAB()
